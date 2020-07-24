ac0e327abced13c3b8a313cf113cc9a4081c46fe
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-07-23 23:47:19 -0400

    base functionality

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -4,44 +4,70 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
+
+import java.util.*;
 
 
 @Controller
 public class SpaDayController {
 
-    public boolean checkSkinType(String skinType, String facialType) {
-        if (skinType.equals("oily")) {
-            if (facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating")) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else if (skinType.equals("combination")) {
-            if (facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating") || facialType.equals("Enzyme Peel")) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else if (skinType.equals("normal")) {
-            return true;
-        }
-        else if (skinType.equals("dry")) {
-            if (facialType.equals("Rejuvenating") || facialType.equals("Hydrofacial")) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            return true;
-        }
+    private static Map<String, List<String>> facialOptions = createMap();
+
+    private static Map<String, List<String>> createMap() {
+        Map<String, List<String>> facialOptions = new HashMap<>();
+        facialOptions.put("oily", Arrays.asList("Microdermabrasion", "Rejuvenating"));
+        facialOptions.put("combination", Arrays.asList("Microdermabrasion", "Rejuvenating", "Enzyme Peel"));
+        facialOptions.put("normal", Arrays.asList("Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel"));
+        facialOptions.put("dry", Arrays.asList("Hydrofacial", "Rejuvenating"));
+        return facialOptions;
     }
+
+    public boolean checkSkinType(String skinType, String facialType) {   // This function is not needed or used but is here for posterity currently
+        return facialOptions.get(skinType).contains(facialType);
+    }
+//        switch (skinType) {
+//            case "oily":
+//                return facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating");
+//            case "combination":
+//                return facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating") || facialType.equals("Enzyme Peel");
+//            case "normal":
+//                return true;
+//            case "dry":
+//                return facialType.equals("Rejuvenating") || facialType.equals("Hydrofacial");
+//        }
+//        return true;
+//    }
+//        if (skinType.equals("oily")) {
+//            if (facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating")) {
+//                return true;
+//            }
+//            else {
+//                return false;
+//            }
+//        }
+//        else if (skinType.equals("combination")) {
+//            if (facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating") || facialType.equals("Enzyme Peel")) {
+//                return true;
+//            }
+//            else {
+//                return false;
+//            }
+//        }
+//        else if (skinType.equals("normal")) {
+//            return true;
+//        }
+//        else if (skinType.equals("dry")) {
+//            if (facialType.equals("Rejuvenating") || facialType.equals("Hydrofacial")) {
+//                return true;
+//            }
+//            else {
+//                return false;
+//            }
+//        }
+//        else {
+//            return true;
+//        }
+//    }
 
     @GetMapping(value="")
     @ResponseBody
@@ -56,32 +82,31 @@ public class SpaDayController {
                 "<option value = 'normal'>Normal</option>" +
                 "<option value = 'dry'>Dry</option>" +
                 "</select><br>" +
-                "Manicure or Pedicure? <br>" +
+                "Manicure and/or Pedicure? <br>" +
                 "<select name = 'manipedi'>" +
                 "<option value = 'manicure'>Manicure</option>" +
                 "<option value = 'pedicure'>Pedicure</option>" +
+//                "<option value = 'both'>Both/ManiPedi</option>" +
                 "</select><br>" +
                 "<input type = 'submit' value = 'Submit'>" +
                 "</form>";
         return html;
     }
 
-    @PostMapping(value="")
+    @PostMapping()
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
-        ArrayList<String> facials = new ArrayList<String>();
-        facials.add("Microdermabrasion");
-        facials.add("Hydrofacial");
-        facials.add("Rejuvenating");
-        facials.add("Enzyme Peel");
-
-        ArrayList<String> appropriateFacials = new ArrayList<String>();
-        for (int i = 0; i < facials.size(); i ++) {
-            if (checkSkinType(skintype,facials.get(i))) {
-                appropriateFacials.add(facials.get(i));
-            }
-        }
-
+//        List<String> facials = Arrays.asList("Microdermabrasion", "Hydrofacial", "Rejuvenating", "Enzyme Peel");
+//        List<String> appropriateFacials = new ArrayList<String>();
+//        for (int i = 0; i < facials.size(); i ++) {
+//            if (checkSkinType(skintype,facials.get(i))) {
+//                appropriateFacials.add(facials.get(i));
+//            }
+//        }
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+        model.addAttribute("appropriateFacials", facialOptions.get(skintype));
         return "menu";
     }
 }
