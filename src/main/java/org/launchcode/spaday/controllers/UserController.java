9c8d70b9f5f0ce75112d03ca2f830b96736e830d
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-07-29 00:46:31 -0400

    if statements everywhere

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,29 @@
+package org.launchcode.spaday.controllers;
+
+import javax.management.modelmbean.ModelMBean;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "/user/add";
+    }
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("username", user.getUserName());
+        if(user.getPassword().equals(verify)) {
+            return "/user/index";
+        }
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("error", "Passwords do not match");
+        return "/user/add";
+    }
+}
---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,27 @@
+package org.launchcode.spaday.models;
+
+public class User {
+
+    private String userName;
+    private String password;
+    private String email;
+
+    public User(String userName, String password, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+        
+}
