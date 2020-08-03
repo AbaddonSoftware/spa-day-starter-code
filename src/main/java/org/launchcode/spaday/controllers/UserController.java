862b3f6ea97978d88a04a24a2c88cbfb38ad44bb
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-08-03 00:28:52 -0400

    added bonus code

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,39 +1,42 @@
 package org.launchcode.spaday.controllers;
 
+import javax.validation.Valid;
 
-
+import org.launchcode.spaday.data.UserData;
 import org.launchcode.spaday.models.User;
-import org.launchcode.spaday.models.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("userName", user.getUserName());
-        if(UserData.getAll().size() > 6) { UserData.clear(); }
-        UserData.add(user);
-        if(user.getPassword().equals(verify)) {
-            model.addAttribute("users", UserData.getAll());
-            return "user/index";
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("errors", errors.getAllErrors());
+            return "user/add";
         }
-        model.addAttribute("error", "Passwords do not match");
-        return "user/add";
+        UserData.add(user);
+        model.addAttribute("users", UserData.getAll());
+        return "user/index";
+
+        
+        
     }
 
     @GetMapping("detail")
     public String userPages(int userID, Model model) {
-        User currentUser = UserData.getById(userID);
-        model.addAttribute("user", currentUser);
+        User user = UserData.getById(userID);
+        model.addAttribute(user);
         return "user/detail";
     }
 
